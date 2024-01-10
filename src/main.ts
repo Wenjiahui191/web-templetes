38085d68ad1c3d41031048bb4339c03ed36b29df
Ric_wen <1913018146@qq.com>
2024-01-10 10:56:07 +0800

    chore: Vue开发模板

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import './main.css'
 
-createApp(App).mount("#app");
+const pinia = createPinia()
+
+createApp(App).use(pinia).mount('#app')
